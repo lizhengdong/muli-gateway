bd87d4e892827efb7f9016466c3ae6898e0b0d4e
unknown <zhengdong@staff.sina.com.cn>
2019-04-20 12:28:24 +0800

    'logs'

---
+++ src/main/java/tv/muli/muligateway/MuliGatewayApplication.java
@@ -0,0 +1,13 @@
+package tv.muli.muligateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuliGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuliGatewayApplication.class, args);
+    }
+
+}
